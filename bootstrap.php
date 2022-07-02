e2c0e814cd3887816df6d4fb2adc9fed41c33420
Dmitry Kolesnikov <kastaneda@gmail.com>
2022-07-02 17:25:42 +0300

    Be more strict

--- bootstrap.php
+++ bootstrap.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 
 error_reporting(E_ALL);
-ini_set('display_errors', 1);
+ini_set('display_errors', '1');
 
 spl_autoload_register(fn($class) =>
     require __DIR__ . '/src/' . strtr($class, '\\', '/') . '.php'
