b4de94d8df7f3c256d175b058e5ae858730c9db2
Dmitry Kolesnikov <kastaneda@gmail.com>
2022-07-08 21:31:56 +0300

    Basic implementation

--- src/Controller/Demo.php
+++ src/Controller/Demo.php
@@ -5,14 +5,36 @@ namespace Controller;
 
 use Model\Post;
 use Model\PostFile;
+use Helper\GitRepo;
 
 class Demo
 {
     public function run(): void
     {
-        $f = new PostFile(new Post($_POST['title'], $_POST['content']));
+        $file = new PostFile(new Post($_POST['title'], $_POST['content']));
+        $repo = $this->getGitRepo();
 
-        echo '<h1>' . htmlspecialchars($f->getFileName()) . '</h1>';
-        echo '<pre>' . htmlspecialchars($f->getFileContent()) . '</pre>';
+        // echo '<h1>' . htmlspecialchars($file->getFileName()) . '</h1>';
+        // echo '<pre>' . htmlspecialchars($file->getFileContent()) . '</pre>';
+
+        $repo->doPull();
+        $file->saveFile();
+        $repo->doCommitAll('New post: ' . $file->post->title);
+        $repo->doPush();
+        echo '<h1>OK</h1>';
+        echo '<pre>' . htmlspecialchars($repo->output) . '</pre>';
+    }
+
+    private function getAppRoot(): string
+    {
+        return dirname(dirname(__DIR__));
+    }
+
+    private function getGitRepo(): GitRepo
+    {
+        $dir = $this->getAppRoot() . '/var/repo';
+        $sshKey = $this->getAppRoot() . '/var/ssh/post_tool';
+
+        return new GitRepo($dir, $sshKey);
     }
 }
